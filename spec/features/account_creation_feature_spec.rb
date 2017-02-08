8c36f5d9408bd336f22cf0b0d944bc9e09492c82
george <george@dringo.org>
2017-02-08 15:17:34 -0800

    working on test site

---
+++ spec/features/account_creation_feature_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'acount creation' do
+  it 'allows user to create account' do
+    cisit root_path
+    click_link 'Create Account'
+
+    fill_in 'Name', with: 'George'
+    fill_in 'Email', with: 'george@dringo.io'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    fill_in 'Subdomain', with: 'test_subdomain'
+    click_button 'Create Account'
+
+    expect(page).to have_content('Signed up successfully')
+  end
+end
