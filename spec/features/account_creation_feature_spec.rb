36dfe20ede6a910cfdbed37b64a42869426a9ec0
george <george@dringo.org>
2017-02-13 19:39:06 -0800

    all specs passing

--- spec/features/account_creation_feature_spec.rb
+++ spec/features/account_creation_feature_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe 'acount creation' do
   it 'allows user to create account' do
     visit root_path
-    click_button 'Create Account'
+    click_link 'Create Account'
 
-    fill_in 'name', with: 'George'
-    fill_in 'email', with: 'george@dringo.io'
-    fill_in 'password', with: 'password'
-    fill_in 'password_confirmation', with: 'password'
-    fill_in 'subdomain', with: 'test_subdomain'
-    click_button 'Submit'
+    fill_in 'Name', with: 'George'
+    fill_in 'Email', with: 'george@dringo.io'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Subdomain', with: 'test_subdomain'
+    click_button 'Create Account'
 
     expect(page).to have_content('Signed up successfully')
   end
