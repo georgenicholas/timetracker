ff35f1811ff23cf2653be85b056bd24cd8bf1e1c
george <george@dringo.org>
2017-02-15 15:11:54 -0800

    all specs in authentication_feature_specs passing

--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -16,7 +16,7 @@ describe 'user authentication' do
     sign_user_in(create(:user))
 
     visit root_path
-    click_link 'Log out'
+    click_link 'Sign out'
     expect(page).to have_content('Signed out successfully.')
   end
 end
