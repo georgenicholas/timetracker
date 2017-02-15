d388f58ea17ada3603c194761fa057b5a478c52e
george <george@dringo.org>
2017-02-15 15:40:31 -0800

    on to video 7

--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -25,5 +25,5 @@ def sign_user_in(user, opts={})
   visit new_user_session_path
   fill_in 'Email', with: user.email
   fill_in 'Password', with: (opts[:password] || user.password)
-  click_button 'Log in'
+  click_button 'Sign in'
 end
