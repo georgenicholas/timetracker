1e8bcb918139916388b9822c80d0ebf581bd9eb4
george <george@dringo.org>
2017-02-16 15:07:38 -0800

    all specs passing at 8:53 of video 10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_filter :load_schema, :authenticate_user!
+  before_filter :load_schema, :authenticate_user!, :set_mailer_host
 
   private
     def load_schema
@@ -21,7 +21,16 @@ class ApplicationController < ActionController::Base
     end
     helper_method :current_account
 
+    def set_mailer_host
+      subdomain = current_account ? "#{current_account.subdomain}." : ""
+      ActionMailer::Base.default_url_options[:host] = "#{subdomain}lvh.me:3000"
+    end
+
     def after_sign_out_path_for(resource_or_scope)
       new_user_session_path
+    end
+
+    def after_invite_path_for(resource)
+      user_path
     end
   end
--- spec/features/invitations_feature_spec.rb
+++ spec/features/invitations_feature_spec.rb
@@ -1,26 +1,26 @@
 require 'spec_helper'
 require 'rails_helper'
-#require 'pry'
 describe 'invitations' do
   let(:user) {build(:user) }
   let!(:account) { create(:account_with_schema, owner: user) }
 
-
-  # let!(:account) { create(:account_with_schema) }
-  # let(:user) { account.owner }
-  #
-  before { set_subdomain(account.subdomain) }
+  before do
+    set_subdomain(account.subdomain)
+    sign_user_in(user, subdomain: account.subdomain)
+    visit users_path
+  end
 
   it 'shows the owner in the authorized users list' do
-    sign_user_in(user, subdomain: account.subdomain)
-
-    # sign_user_in(user)
-    #binding.pry
-    visit users_path
-    #binding.pry
     expect(page).to have_content user.name
     expect(page).to have_content user.email
     expect(page).to have_selector '.glyphicon-ok'
+  end
 
+  it 'shows invitation when user is invited' do
+    fill_in 'Email', with: 'ryan@tanookilabs.com'
+    click_button 'Invite User'
+    expect(page).to have_content 'invitation email hsa been sent'
+    expect(page).to have_content 'ryan@tanookilabs.com'
+    expect(page).to have_content 'Invitation Pending'
   end
 end
