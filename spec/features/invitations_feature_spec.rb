a3fe24d12df919cd964bb40a3d6721ad3a42e05f
george <george@dringo.org>
2017-02-16 11:55:36 -0800

    site is working, but one of the specs isnt passing yet for video 8

--- spec/features/invitations_feature_spec.rb
+++ spec/features/invitations_feature_spec.rb
@@ -2,13 +2,19 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe 'invitations' do
-  let!(:account) { create(:account_with_schema) }
-  let(:user) { account.owner }
+  let(:user) {build(:user) }
+  let!(:account) { create(:account_with_schema, owner: user) }
 
-  before { set_subdomain(account.subdomain) }
+
+  # let!(:account) { create(:account_with_schema) }
+  # let(:user) { account.owner }
+  #
+  # before { set_subdomain(account.subdomain) }
 
   it 'shows the owner in the authorized users list' do
-    sign_user_in(user)
+    sign_user_in(user, subdomain: account.subdomain)
+
+    # sign_user_in(user)
     visit users_path
     expect(page).to have_content user.name
     expect(page).to have_content user.email
