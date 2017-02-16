7b1c3d728f2a408cb864ad424e97c30d4fa695df
george <george@dringo.org>
2017-02-15 20:06:49 -0800

    all rspecs passing, though not sure how I fixed it

--- spec/features/account_creation_feature_spec.rb
+++ spec/features/account_creation_feature_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
+require 'rails_helper'
 
 describe 'acount creation' do
+  let(:subdomain) { FactoryGirl.generate(:subdomain) }
+  before(:each) { sign_up(subdomain) }
+
   it 'allows user to create account' do
-    visit root_path
+    expect(page.current_url).to include(subdomain)
+    expect(Account.all.count).to eq(1)
+  end
+
+  it 'allows access of subdomain' do
+    visit root_url(subdomain: subdomain)
+    expect(page.current_url).to include(subdomain)
+  end
+
+  def sign_up(subdomain)
+    visit root_url(subdomain: false)
     click_link 'Create Account'
 
     fill_in 'Name', with: 'George'
     fill_in 'Email', with: 'george@dringo.io'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
-    fill_in 'Subdomain', with: 'test_subdomain'
+    fill_in 'Subdomain', with: subdomain
     click_button 'Create Account'
-
-    expect(page).to have_content('Signed up successfully')
   end
 end
