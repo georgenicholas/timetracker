8275a16d88aa18ddb09b3a2ab65a7681f4a8f794
george <george@dringo.org>
2017-02-16 11:12:57 -0800

    tseting something

---
+++ spec/features/invitations_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'invitations' do
+  let!(:account) { create(:account_with_schema) }
+  let(:user) { account.owner }
+
+  it 'shows the owner in the authorized users list' do
+    sign_user_in(user, subdomain: account.subdomain)
+    visit users_url(subdomain: account.subdomain)
+    expect(page).to have_content user.name
+    expect(page).to have_selector '.glyphicon'
+  end
+end
