ba0ca6a87d47fbb51fdefcb64cd6cf6012b31a86
george <george@dringo.org>
2017-02-15 18:12:16 -0800

    fixed an rpsec.  there are no failing specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+class SubdomainPresent
+  def self.matches?(request)
+    request.subdomain.present?
+  end
+end
+
 Rails.application.routes.draw do
-  devise_for :users
+  constraints(SubdomainPresent) do
+    devise_for :users
+  end
+
   root :to => 'welcome#index'
-  #get "accounts/new", to: "accounts#new"
   resources :accounts
 end
--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -2,27 +2,34 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe 'user authentication' do
+  let(:user) {build(:user) }
+  let!(:account) { create(:account, owner: user) }
+
   it 'allows signin with valid credentials' do
-    sign_user_in(create(:user))
+    sign_user_in(user, subdomain: account.subdomain)
     expect(page).to have_content('Signed in successfully.')
   end
 
   it 'does not allow signin with invalid credentials' do
-    sign_user_in(create(:user), password: 'wrong password')
+    sign_user_in(user, subdomain: account.subdomain, password: 'wrong password')
     expect(page).to have_content('Invalid Email or password.')
   end
 
-  it 'allows user to sign out' do
-    sign_user_in(create(:user))
+  it 'does not allow user to sign in unless on subdomain' do
+    visit new_user_session_path
+    expect(page).to have_content('Routing Error')
+  end
 
-    visit root_path
+  it 'allows user to sign out' do
+    sign_user_in(user, subdomain: account.subdomain)
+
     click_link 'Sign out'
     expect(page).to have_content('Signed out successfully.')
   end
 end
 
 def sign_user_in(user, opts={})
-  visit new_user_session_path
+  visit new_user_session_url(subdomain: opts[:subdomain])
   fill_in 'Email', with: user.email
   fill_in 'Password', with: (opts[:password] || user.password)
   click_button 'Sign in'
