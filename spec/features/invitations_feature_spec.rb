8b512b15391c56d5d03a636b948bf43fcc04f56e
george <george@dringo.org>
2017-02-16 14:18:30 -0800

    fixed all rspecs, done with video 8

--- spec/features/invitations_feature_spec.rb
+++ spec/features/invitations_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require 'rails_helper'
-
+#require 'pry'
 describe 'invitations' do
   let(:user) {build(:user) }
   let!(:account) { create(:account_with_schema, owner: user) }
@@ -9,15 +9,18 @@ describe 'invitations' do
   # let!(:account) { create(:account_with_schema) }
   # let(:user) { account.owner }
   #
-  # before { set_subdomain(account.subdomain) }
+  before { set_subdomain(account.subdomain) }
 
   it 'shows the owner in the authorized users list' do
     sign_user_in(user, subdomain: account.subdomain)
 
     # sign_user_in(user)
+    #binding.pry
     visit users_path
+    #binding.pry
     expect(page).to have_content user.name
     expect(page).to have_content user.email
     expect(page).to have_selector '.glyphicon-ok'
+
   end
 end
