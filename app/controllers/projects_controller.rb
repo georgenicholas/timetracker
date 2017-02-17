7bab52c47119951104246be91ff213af3595c185
george <george@dringo.org>
2017-02-17 12:15:33 -0800

    new stuff, no new specs yet, 7:54 of video 11

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,24 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+  end
+
+  def create
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path, notice: "Project created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :client, :archived)
   end
 end
